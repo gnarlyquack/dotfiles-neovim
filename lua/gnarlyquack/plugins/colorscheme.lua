952b6ad86155d89e5edc3300d7676dbdbe2d222b
Karl Nack <karl.nack@fastmail.com>
2024-03-26 23:57:02 -0500

    Move color schemes to plugins

--- lua/gnarlyquack/plugins/colorscheme.lua
+++ lua/gnarlyquack/plugins/colorscheme.lua
@@ -1,7 +1,44 @@
 return {
-    "kvrohit/substrata.nvim",
-    priority = 1000,
-    config = function()
-        vim.cmd [[ colorscheme substrata ]]
-    end,
+    {
+        "thallada/farout.nvim",
+        --"fcpg/vim-farout",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "farout"
+        end,
+    },
+    {
+        "kvrohit/substrata.nvim",
+        event = "VeryLazy",
+        --[[
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "base16-grayscale-dark"
+        end,
+        --]]
+    },
+    {
+        "tinted-theming/base16-vim",
+        event = "VeryLazy",
+        --[[
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "base16-grayscale-dark"
+        end,
+        --]]
+    },
+    {
+        "cocopon/iceberg.vim",
+        event = "VeryLazy",
+        --[[
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "iceberg"
+        end,
+        --]]
+    },
 }
