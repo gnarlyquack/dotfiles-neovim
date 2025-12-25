4e2b8edae7fcf9d53f3534839a5f8018dc8268a4
Karl Nack <karl.nack@fastmail.com>
2025-12-25 02:36:58 -0600

    Fix tree-sitter configuration

--- lua/gnarlyquack/plugins/treesitter.lua
+++ lua/gnarlyquack/plugins/treesitter.lua
@@ -1,43 +1,37 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    config = function ()
-        local configs = require("nvim-treesitter.configs")
+    config = function()
+        local ts = require("nvim-treesitter")
+        parsers = {
+            "bash",
+            "c",
+            "cmake",
+            "comment", -- comment tags
+            "cpp",
+            "git_config",
+            "git_rebase",
+            "gitattributes",
+            "gitcommit",
+            "gitignore",
+            "lua",
+            "make",
+            "python",
+            "rust",
+            "sql",
+            "toml",
+            "vim",
+            "vimdoc", -- vim documentation
+        }
 
-        configs.setup({
-            ensure_installed = {
-                "bash",
-                "c",
-                "cmake",
-                "comment", -- comment tags
-                "cpp",
-                "git_config",
-                "git_rebase",
-                "gitattributes",
-                "gitcommit",
-                "gitignore",
-                "lua",
-                "make",
-                "python",
-                "rust",
-                "sql",
-                "toml",
-                "vim",
-                "vimdoc", -- vim documentation
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
+        ts.install(parsers)
 
-                -- Setting this to true will run `:h syntax` and tree-sitter
-                -- at the same time. Set this to `true` if you depend on
-                -- 'syntax' being enabled (like for indentation). Using this
-                -- option may slow down your editor, and you may see some
-                -- duplicate highlights. Instead of true it can also be a
-                -- list of languages
-                additional_vim_regex_highlighting = false,
-            },
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = parsers,
+            callback = function()
+                vim.treesitter.start()
+            end
         })
-    end
+    end,
 }
